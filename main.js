114a67e25855b26ec4694885593259f1685ba2d9
ntsenguashvili <nat.tsenguashvili@gmail.com>
2024-01-18 17:07:05 +0400

    created js test
    - added variables
    - added math opertions
    - added strings
    - added area

---
+++ main.js
@@ -0,0 +1,51 @@
+
+let a =5;
+let b =2;
+let c = "string";
+
+
+let decimal = 0.5;
+let myBool = true;
+
+
+const myObject = {
+    length: 25,
+    green: false,
+    red: true
+}
+
+const myArray = [1,2,3,4,5,6,7,8,9]
+
+
+const date = new Date("2024-01-18");
+
+
+
+///  End of variables:
+
+
+// Math
+
+let x = 5; y = 34234;
+
+let sum = x+y;
+let mul = x*y;
+let sqr = x*x;
+let div = x/y;
+let combo = ( (x+y) * (x-y) ) / 4
+//concat:
+
+let d = `custom ${c}`;
+
+let concatString = date + ' ' + c + ' ' + d;
+
+
+
+
+console.log(concatString);
+
+//Area
+
+let length = 7;
+let width = 5; 
+let area = length * width;
